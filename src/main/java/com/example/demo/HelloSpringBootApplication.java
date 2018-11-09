4efcb76234ad16d88095bb4f645d6b470978bc76
dactue1006 <dactue1006@gmail.com>
2018-11-09 23:28:01 +0700

    deploy to tomcat

--- src/main/java/com/example/demo/HelloSpringBootApplication.java
+++ src/main/java/com/example/demo/HelloSpringBootApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class HelloSpringBootApplication {
+public class HelloSpringBootApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(HelloSpringBootApplication.class);
+   }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringBootApplication.class, args);
@@ -15,6 +22,6 @@ public class HelloSpringBootApplication {
 	
 	@RequestMapping(value = "/")
 	public String Hello() {
-		return "Hello World";
+		return "Hello from Tom Cat";
 	}
 }
