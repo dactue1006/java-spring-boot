78f19cf5d3bc8b417ba3a2fbdd791dd90df623c4
dactue1006 <dactue1006@gmail.com>
2018-11-10 21:20:56 +0700

    using service component

--- src/main/java/com/example/demo/controller/ProductServiceController.java
+++ src/main/java/com/example/demo/controller/ProductServiceController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,44 +14,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.exception.ProductNotFoundException;
 import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
 
 @RestController
 public class ProductServiceController {
-	private static Map<String, Product> productRepo = new HashMap<>();
-	static {
-		Product honey = new Product();
-		honey.setId("1");
-		honey.setName("Honey");
-		productRepo.put(honey.getId(), honey);
-		
-		Product almond = new Product();
-		almond.setId("2");
-		almond.setName("Almond");
-		productRepo.put(almond.getId(), almond);
-	}
-	
+	@Autowired
+	ProductService productService;
 	@RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Object> delete(@PathVariable("id") String id) {
-		productRepo.remove(id);
+		productService.deleteProduct(id);
 		return new ResponseEntity<>("Product is deleted successfully", HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
-		if (!productRepo.containsKey(id)) throw new ProductNotFoundException();
-		productRepo.remove(id);
-		product.setId(id);
-		productRepo.put(id, product);
+		//if (!productRepo.containsKey(id)) throw new ProductNotFoundException();
+		productService.updateProduct(id, product);
 		return new ResponseEntity<>("Product is updated successfully", HttpStatus.OK);
 	}
 	@RequestMapping(value = "/products", method = RequestMethod.POST)
 	public ResponseEntity<Object> createProduct(@RequestBody Product product) {
-		productRepo.put(product.getId(), product);
+		productService.createProduct(product);
 		return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
 	}
 	
 	@RequestMapping(value = "/products")
 	public ResponseEntity<Object> getProduct() {
-		return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+		return new ResponseEntity<>(productService.getProduct(), HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import java.util.Collection;
+
+import com.example.demo.model.Product;
+
+public interface ProductService {
+	void createProduct(Product product);
+	void updateProduct(String id, Product product);
+	void deleteProduct(String id);
+	Collection<Product> getProduct();
+}
